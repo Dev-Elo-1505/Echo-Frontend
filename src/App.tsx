086692ef927685813c8232d516931dfab0ad6a43
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-03-19 12:51:42 +0100

    chore: setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet,  useLocation } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div><Outlet /></div>;
+  const location = useLocation();
+
+  const hideHeader =
+    location.pathname === "/auth/signup" || location.pathname === "/auth/login";
+  return (
+    <div>
+      {!hideHeader && <Header />}
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+  return (
+    <header className="bg-primary">
+        <div className="text-green-500">Logo</div>
+        <div>SearchBar</div>
+        <div>User</div>
+    </header>
+  )
+}
+
+export default Header
