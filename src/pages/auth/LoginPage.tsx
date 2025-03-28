147fe8ec6b4e6747042f697fa6bf278ee2b70c58
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2025-03-28 23:53:30 +0100

    feat: build login skeleton ui

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,9 +1,22 @@
-
-
+import bg from "../../assets/picbg.jpg";
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div
+      className="w-full min-h-screen bg-cover bg-center
+                  "
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <h1 className="text-white text-4xl font-bold text-center md:text-left">Echo</h1>
+      <div className="flex justify-center items-center h-screen">
+        <div className="bg-white w-auto p-5 rounded-xl flex justify-between items-center">
+          <div className="hidden md:block bg-primary w-1/2 rounded-xl p-2">
+            <h2 className="font-semibold">Bridging the gap between Students and Management</h2>
+          </div>
+          <div>hello</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
